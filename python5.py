4fdc22105f5d9351536994139de66aca6101de2f
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-12 21:24:39 +0530

    condition

--- python5.py
+++ python5.py
@@ -2,4 +2,6 @@ candidates = ["Alice", "Bob", "Charlie"]
 votes = {name: 0 for name in candidates}
 def display_candidates():
     print("\nCandidates:")
+ for i, name in enumerate(candidates, 1):
+        print(f"{i}. {name}")
 
