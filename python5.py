5cf7cee9f89da589b83c5e0e743a95f06b1ee18b
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-12 21:26:30 +0530

    condition1

--- python5.py
+++ python5.py
@@ -4,4 +4,6 @@ def display_candidates():
     print("\nCandidates:")
  for i, name in enumerate(candidates, 1):
         print(f"{i}. {name}")
-
+def vote():
+    display_candidates()
+    choice = int(input("Enter the number of your chosen candidate: "))
