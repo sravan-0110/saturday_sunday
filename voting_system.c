5bdbcb35a681d790c94c8fc9905c44bad81cdb65
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-13 21:19:30 +0530

    insert

--- voting_system.c
+++ voting_system.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 
 int votesA = 0, votesB = 0, votesC = 0, spoiledVotes = 0;
-
+void castVote() {
+    int choice;
+    printf("\nVote for your candidate:\n");
+    printf("1. Candidate A\n");
+    printf("2. Candidate B\n");
+    printf("3. Candidate C\n");
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &choice);
